8e3b0210111ea077cce53ee55acb37a7d4801e2e
truongpx-Mac <truongpx396@gmail.com>
2023-12-01 16:10:32 +0700

    update

--- 01-ekscluster-terraform-manifests/eks-node-group-public.tf
+++ 01-ekscluster-terraform-manifests/eks-node-group-public.tf
@@ -23,8 +23,8 @@ resource "aws_eks_node_group" "eks_ng_public" {
   # }
 
   scaling_config {
-    desired_size = 3
-    min_size     = 3    
+    desired_size = 4
+    min_size     = 4    
     max_size     = 4
   }
 
