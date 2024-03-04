88a4047c0a5e7f5d2d1fa8d7b456a072d2bd04d6
truongpx-Mac <truongpx396@gmail.com>
2024-03-04 16:33:03 +0700

    update

--- 01-ekscluster-terraform-manifests/c7-01-kubernetes-provider.tf
+++ 01-ekscluster-terraform-manifests/c7-01-kubernetes-provider.tf
@@ -7,12 +7,12 @@ data "aws_eks_cluster_auth" "cluster" {
 provider "kubernetes" {
   host = aws_eks_cluster.eks_cluster.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  # token = data.aws_eks_cluster_auth.cluster.token
-   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
-    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
-    command = "aws"
-  }
+  token = data.aws_eks_cluster_auth.cluster.token
+  #  exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+  #   command = "aws"
+  # }
 }
 
 resource "kubernetes_namespace" "openim" {
