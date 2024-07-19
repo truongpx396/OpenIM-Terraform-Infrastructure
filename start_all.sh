591410a7af93a8159daa2974d404894d219f394f
truongpx-Mac <truongpx396@gmail.com>
2024-07-19 13:05:41 +0700

    udpate

--- start_all.sh
+++ start_all.sh
@@ -5,4 +5,5 @@ cd 01-ekscluster-terraform-manifests && terraform apply -auto-approve \
 && cd ../05-argocd-terraform-manifests && terraform apply -auto-approve \
 && cd .. \
 && aws eks --region ap-southeast-1 update-kubeconfig --name magiclab396-dev-openim-cluster-1 \
-&& kubectl create -n istio-system secret tls myk8s-magiclab396-credential --key=openim.k8s.magiclab396.com.key --cert=openim.k8s.magiclab396.com.crt
+&& kubectl create -n istio-system secret tls myk8s-magiclab396-credential --key=openim.k8s.magiclab396.com.key --cert=openim.k8s.magiclab396.com.crt \
+&& kubectl get secret -n argocd argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 --d
