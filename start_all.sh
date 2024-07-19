d227272eac8e55463fd300e61638ed26cf429cb7
truongpx-Mac <truongpx396@gmail.com>
2024-07-19 11:20:01 +0700

    udpate

--- start_all.sh
+++ start_all.sh
@@ -1,1 +1,7 @@
-cd 01-ekscluster-terraform-manifests && terraform apply -auto-approve && cd ../02-ebs-terraform-manifests && terraform apply -auto-approve && cd ../03-externaldns-terraform-manifests && terraform apply -auto-approve && cd ../04-lbc-terraform-manifests && terraform apply -auto-approve && cd ../05-argocd-terraform-manifests && terraform apply -auto-approve && cd .. && aws eks --region ap-southeast-1 update-kubeconfig --name magiclab396-dev-openim-cluster-1
+cd 01-ekscluster-terraform-manifests && terraform apply -auto-approve \
+&& cd ../02-ebs-terraform-manifests && terraform apply -auto-approve \
+&& cd ../03-externaldns-terraform-manifests && terraform apply -auto-approve \
+&& cd ../04-lbc-terraform-manifests && terraform apply -auto-approve \
+&& cd ../05-argocd-terraform-manifests && terraform apply -auto-approve \
+&& cd .. \
+&& aws eks --region ap-southeast-1 update-kubeconfig --name magiclab396-dev-openim-cluster-1
